2869fa75aeba89b60f6bc252c084f88c6bf420b8
Cora Sutton <cora@acuitymd.com>
2025-07-09 17:23:55 -0500

    platform-specific lib load paths

--- build.rs
+++ build.rs
@@ -1,6 +1,13 @@
 fn main() {
-    // support shared libs from homebrew
-    println!("cargo:rustc-link-search=/opt/homebrew/lib");
+    if cfg!(target_os = "macos") {
+        // support shared libs from homebrew
+        println!("cargo:rustc-link-search=/opt/homebrew/lib");
+    }
+
+    if cfg!(target_os = "linux") {
+        // support shared libs from apt
+        println!("cargo:rustc-link-search=/usr/local/lib");
+    }
 
     // link the duckdb library
     println!("cargo:rustc-link-lib=duckdb");
