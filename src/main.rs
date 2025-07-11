74d9db12b0c5d5fde4a65035c38dd66f91308bb9
Cora Sutton <cora@acuitymd.com>
2025-07-11 17:40:39 -0500

    Get rid of prefix

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
 use anyhow::Result;
 use clap::Parser;
-use silk_chiffon::{Cli, commands};
+use silk_chiffon::{Cli, Commands, commands};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let cli = Cli::parse();
 
     match cli.command {
-        silk_chiffon::Commands::Parquet(args) => commands::parquet::run(args).await?,
-        silk_chiffon::Commands::Duckdb(args) => commands::duckdb::run(args).await?,
-        silk_chiffon::Commands::Arrow(args) => commands::arrow::run(args).await?,
-        silk_chiffon::Commands::SplitToArrow(args) => commands::split_to_arrow::run(args).await?,
-        silk_chiffon::Commands::SplitToParquet(args) => {
-            commands::split_to_parquet::run(args).await?
-        }
+        Commands::Parquet(args) => commands::parquet::run(args).await?,
+        Commands::Duckdb(args) => commands::duckdb::run(args).await?,
+        Commands::Arrow(args) => commands::arrow::run(args).await?,
+        Commands::SplitToArrow(args) => commands::split_to_arrow::run(args).await?,
+        Commands::SplitToParquet(args) => commands::split_to_parquet::run(args).await?,
     };
     Ok(())
 }
