0198ab60960acd4fac61a3a8bc485e6a29329858
Cora Sutton <cora@acuitymd.com>
2025-07-17 14:04:13 -0500

    Add new python functions for merge functions

--- src/python/mod.rs
+++ src/python/mod.rs
@@ -3,6 +3,9 @@ use pyo3::prelude::*;
 mod arrow;
 mod common;
 mod duckdb;
+mod merge_to_arrow;
+mod merge_to_duckdb;
+mod merge_to_parquet;
 mod parquet;
 mod split_to_arrow;
 mod split_to_parquet;
@@ -13,5 +16,8 @@ pub fn register_module(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(duckdb::arrow_to_duckdb, m)?)?;
     m.add_function(wrap_pyfunction!(split_to_arrow::split_to_arrow, m)?)?;
     m.add_function(wrap_pyfunction!(split_to_parquet::split_to_parquet, m)?)?;
+    m.add_function(wrap_pyfunction!(merge_to_arrow::merge_to_arrow, m)?)?;
+    m.add_function(wrap_pyfunction!(merge_to_parquet::merge_to_parquet, m)?)?;
+    m.add_function(wrap_pyfunction!(merge_to_duckdb::merge_to_duckdb, m)?)?;
     Ok(())
 }
