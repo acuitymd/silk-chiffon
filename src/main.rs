e38609fe5180945f14b08ed17ab3d7c58f48b794
Cora Sutton <cora@acuitymd.com>
2025-08-16 01:59:03 -0500

    import

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,20 @@
 use anyhow::Result;
 use clap::Parser;
-use silk_chiffon::{Cli, commands};
+use silk_chiffon::{Cli, Commands, commands};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let cli = Cli::parse();
 
     match cli.command {
-        silk_chiffon::Commands::Parquet(args) => commands::parquet::run(args).await?,
-        silk_chiffon::Commands::Duckdb(args) => commands::duckdb::run(args).await?,
-        silk_chiffon::Commands::Arrow(args) => commands::arrow::run(args).await?,
-        silk_chiffon::Commands::SplitToArrow(args) => commands::split_to_arrow::run(args).await?,
-        silk_chiffon::Commands::SplitToParquet(args) => {
-            commands::split_to_parquet::run(args).await?
-        }
-        silk_chiffon::Commands::MergeToArrow(args) => commands::merge_to_arrow::run(args).await?,
-        silk_chiffon::Commands::MergeToParquet(args) => {
-            commands::merge_to_parquet::run(args).await?
-        }
-        silk_chiffon::Commands::MergeToDuckdb(args) => commands::merge_to_duckdb::run(args).await?,
+        Commands::Parquet(args) => commands::parquet::run(args).await?,
+        Commands::Duckdb(args) => commands::duckdb::run(args).await?,
+        Commands::Arrow(args) => commands::arrow::run(args).await?,
+        Commands::SplitToArrow(args) => commands::split_to_arrow::run(args).await?,
+        Commands::SplitToParquet(args) => commands::split_to_parquet::run(args).await?,
+        Commands::MergeToArrow(args) => commands::merge_to_arrow::run(args).await?,
+        Commands::MergeToParquet(args) => commands::merge_to_parquet::run(args).await?,
+        Commands::MergeToDuckdb(args) => commands::merge_to_duckdb::run(args).await?,
     };
     Ok(())
 }
