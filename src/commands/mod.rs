f36ffc2b39eab58d467c9dd3646da02b51f02c0f
Cora Sutton <cora@sutton.me>
2025-11-26 22:15:01 -0600

    Remove old commands (#51)
    * More tests
    
    * Remove old commands
    
    * Improve the readme
    
    * remove unused deps
    
    * Whoops, one more dep

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,7 +1,1 @@
-pub mod arrow_to_arrow;
-pub mod arrow_to_parquet;
-pub mod merge_arrow_to_arrow;
-pub mod merge_arrow_to_parquet;
-pub mod partition_arrow_to_arrow;
-pub mod partition_arrow_to_parquet;
 pub mod transform;
