bb903411a4eca9853188bc549097adee220ccb31
Cora Sutton <cora@acuitymd.com>
2026-01-07 17:44:09 -0600

    Latest datafusion and make stuff public

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,11 +1,9 @@
 pub mod arrow_versioning;
 pub mod collections;
 pub mod filesystem;
-pub(crate) mod first_error;
-pub(crate) mod ordered_channel;
-pub(crate) mod ordered_demux;
+pub mod first_error;
+pub mod ordered_channel;
+pub mod ordered_demux;
 pub mod parquet_inspection;
 pub mod projected_stream;
 pub mod test_helpers;
-
-pub(crate) use ordered_demux::{OrderedDemuxConfig, OrderedDemuxExt};
