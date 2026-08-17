83f26b8a16a09dcb5a2e1776575d1fbbe5b88916
Cora Sutton <cora@acuitymd.com>
2026-08-17 07:16:33 -0600

    Revert "Add format registration contracts (#102)"
    This reverts commit 3870c5a2be5c6d9ebce443cd9612a7c5534c37c2.

--- crates/silk-chiffon-core/src/inspection.rs
+++
@@ -1,8 +0,0 @@
-use serde_json::Value;
-
-/// Rendered inspection output returned by a format.
-#[derive(Clone, Debug, PartialEq)]
-pub enum InspectionOutput {
-    Text(String),
-    Json(Value),
-}
